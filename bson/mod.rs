0ed0f4727e2b6b1c07ca6182ade2061f368340f4
Kai Lieth <klieth@live.unc.edu>
2014-07-29 17:03:45 -0700

    ObjectId support

--- bson/mod.rs
+++ bson/mod.rs
@@ -1,11 +1,71 @@
 
 use std::fmt::{Show,Formatter,FormatError};
 use std::collections::treemap::{TreeMap, Entries};
+use std::slice::Items;
 
 pub enum BSONData {
     BSONObject(BSON),
     BSONInt(i32),
     BSONString(String),
+    BSONId(Vec<u8>),
+}
+impl BSONData {
+    fn get_name(it: &mut Items<u8>) -> String {
+        let mut name = String::new();
+        loop {
+            let c = *it.next().unwrap();
+            match c {
+                0x00 => break,
+                _ => name.push_char(c as char),
+            }
+        }
+        name
+    }
+    fn deserialize_int(it: &mut Items<u8>) -> (String, BSONData) {
+        let mut name = BSONData::get_name(it);
+        let num: i32 = *it.next().unwrap() as i32 
+            | (*it.next().unwrap() as i32 << 8) 
+            | (*it.next().unwrap() as i32 << 16) 
+            | (*it.next().unwrap() as i32 << 24);
+        (name, BSONInt(num))
+    }
+    fn deserialize_string(it: &mut Items<u8>) -> (String, BSONData) {
+        let mut name = BSONData::get_name(it);
+        let len: i32 = *it.next().unwrap() as i32 
+            | (*it.next().unwrap() as i32 << 8) 
+            | (*it.next().unwrap() as i32 << 16) 
+            | (*it.next().unwrap() as i32 << 24);
+        let mut s = String::new();
+        for _ in range(0,len-1) {
+            s.push_char((*it.next().unwrap()) as char);
+        }
+        it.next().unwrap();
+        (name, BSONString(s))
+    }
+    fn deserialize_object(it: &mut Items<u8>) -> (String, BSONData) {
+        let mut name = BSONData::get_name(it);
+        let mut vec: Vec<u8> = Vec::new();
+        vec.push(*it.next().unwrap());
+        vec.push(*it.next().unwrap());
+        vec.push(*it.next().unwrap());
+        vec.push(*it.next().unwrap());
+        let len: i32 = vec[0] as i32 
+            | (vec[1] as i32 << 8) 
+            | (vec[2] as i32 << 16) 
+            | (vec[3] as i32 << 24);
+        for _ in range(4,len) {
+            vec.push(*it.next().unwrap());
+        }
+        (name, BSONObject(BSON::deserialize(vec)))
+    }
+    fn deserialize_id(it: &mut Items<u8>) -> (String, BSONData) {
+        let mut name = BSONData::get_name(it);
+        let mut id = Vec::new();
+        for _ in range(0u,12) {
+            id.push(*it.next().unwrap());
+        }
+        (name, BSONId(id))
+    }
 }
 impl Show for BSONData {
     fn fmt(&self, f: &mut Formatter) -> Result<(),FormatError> {
@@ -13,7 +73,42 @@ impl Show for BSONData {
             BSONObject(ref o) => write!(f, "Object: {}", o),
             BSONInt(ref i) => write!(f, "Number: {}", i),
             BSONString(ref s) => write!(f, "String: {}", s),
+            BSONId(ref i) => write!(f, "ObjectId: {}", i),
         }
+    }
+}
+impl PartialEq for BSONData {
+    fn eq(&self, other: &BSONData) -> bool {
+        match *self {
+            BSONObject(ref o1) => {
+                match *other {
+                    BSONObject(ref o2) => o1.eq(o2),
+                    _ => false
+                }
+            },
+            BSONInt(ref i1) => {
+                match *other {
+                    BSONInt(ref i2) => i1.eq(i2),
+                    _ => false,
+                }
+            },
+            BSONString(ref s1) => {
+                match *other {
+                    BSONString(ref s2) => s1.eq(s2),
+                    _ => false,
+                }
+            },
+            BSONId(ref i1) => {
+                match *other {
+                    BSONId(ref i2) => i1.eq(i2),
+                    _ => false
+                }
+            }
+        }
+    }
+    fn ne(&self, other: &BSONData) -> bool {
+        unimplemented!();
+        false
     }
 }
 
@@ -23,6 +118,44 @@ pub struct BSON {
 impl BSON {
     pub fn new() -> BSON {
         BSON { elements: TreeMap::new() }
+    }
+    pub fn deserialize(data: Vec<u8>) -> BSON {
+        let mut result = BSON { elements: TreeMap::new() };
+        let mut it = data.iter();
+        println!("Vec: {}", data);
+        let bytes: uint = *it.next().unwrap() as uint 
+            | (*it.next().unwrap() as uint << 8) 
+            | (*it.next().unwrap() as uint << 16) 
+            | (*it.next().unwrap() as uint << 24);
+        println!("Bytes: {}", bytes);
+        loop {
+            match *it.next().unwrap() {
+                0x02 => {
+                    match BSONData::deserialize_string(&mut it) {
+                        (name, value) => result.insert(name, value),
+                    }
+                },
+                0x03 => {
+                    match BSONData::deserialize_object(&mut it) {
+                        (name, value) => result.insert(name, value),
+                    }
+                },
+                0x07 => {
+                    match BSONData::deserialize_id(&mut it) {
+                        (name, value) => result.insert(name, value),
+                    }
+                },
+                0x10 => {
+                    match BSONData::deserialize_int(&mut it) {
+                        (name, value) => result.insert(name, value),
+                    }
+                },
+                0x00 => break,
+                _ => unimplemented!(),
+            }
+        }
+        println!("Result: {}", result);
+        result
     }
     pub fn insert(&mut self, key: String, data: BSONData) {
         self.elements.insert(key, data);
@@ -77,7 +210,18 @@ impl BSON {
                                 local.push(c as u8);
                             }
                             local.push(0x00);
-                        }
+                        },
+                        BSONId(ref i) => {
+                            local.push(0x07);
+                            for c in name.as_slice().chars() {
+                                local.push(c as u8);
+                            }
+                            local.push(0x00);
+                            assert_eq!(i.len(), 12u);
+                            for v in i.iter() {
+                                local.push(*v);
+                            }
+                        },
                     }
                     v.push(local);
                 },
@@ -98,5 +242,24 @@ impl Show for BSON {
             try!(write!(f, "{}", v));
         }
         write!(f, "\n")
+    }
+}
+impl PartialEq for BSON {
+    fn eq(&self, other: &BSON) -> bool {
+        if self.elements.len() != other.elements.len() {
+            return false;
+        }
+        let mut s = self.elements.iter();
+        let mut o = other.elements.iter();
+        for i in range(0,self.elements.len()) {
+            if !s.next().unwrap().eq(&o.next().unwrap()) {
+                return false;
+            }
+        }
+        true
+    }
+    fn ne(&self, other: &BSON) -> bool {
+        unimplemented!();
+        false
     }
 }
--- bson_test.rs
+++ bson_test.rs
@@ -1,7 +1,7 @@
 
 use std::io::File;
 
-use bson::{BSON,BSONString,BSONInt,BSONObject};
+use bson::{BSON,BSONString,BSONInt,BSONObject,BSONId};
 
 
 mod bson;
@@ -27,15 +27,21 @@ fn write_bson(v: &Vec<u8>, name: &str) {
 }
 
 #[test]
-fn empty() {
+fn serialize_empty() {
     let empty = BSON::new();
     let serialized = empty.serialize();
     show_bson(&serialized);
     assert_eq!(serialized, vec!(0x05,0x00,0x00,0x00,0x00));
 }
+#[test]
+fn deserialize_empty() {
+    let empty = BSON::new();
+    let built = BSON::deserialize(vec!(0x05,0x00,0x00,0x00,0x00));
+    assert_eq!(empty, built);
+}
 
 #[test]
-fn int32() {
+fn serialize_int32() {
     let mut bson = BSON::new();
     bson.insert("int32".to_string(), BSONInt(10));
     let serialized = bson.serialize();
@@ -46,9 +52,20 @@ fn int32() {
                                 0x0A,0x00,0x00,0x00,
                                 0x00));
 }
+#[test]
+fn deserialize_int32() {
+    let mut correct = BSON::new();
+    correct.insert("int32".to_string(), BSONInt(10));
+    let built = BSON::deserialize(vec!(0x10,0x00,0x00,0x00,
+                                       0x10,
+                                       0x69,0x6e,0x74,0x33,0x32,0x00,
+                                       0x0A,0x00,0x00,0x00,
+                                       0x00));
+    assert_eq!(correct, built);
+}
 
 #[test]
-fn string() {
+fn serialize_string() {
     let mut bson = BSON::new();
     bson.insert("string".to_string(), BSONString("sample".to_string()));
     let serialized = bson.serialize();
@@ -60,9 +77,21 @@ fn string() {
                                 0x73,0x61,0x6d,0x70,0x6c,0x65,0x00,
                                 0x00));
 }
+#[test]
+fn deserialize_string() {
+    let mut correct = BSON::new();
+    correct.insert("string".to_string(), BSONString("sample".to_string()));
+    let built = BSON::deserialize(vec!(0x18,0x00,0x00,0x00,
+                                       0x02,
+                                       0x73,0x74,0x72,0x69,0x6e,0x67,0x00,
+                                       0x07,0x00,0x00,0x00,
+                                       0x73,0x61,0x6d,0x70,0x6c,0x65,0x00,
+                                       0x00));
+    assert_eq!(correct, built);
+}
 
 #[test]
-fn embedded() {
+fn serialize_embedded() {
     let mut embed = BSON::new();
     embed.insert("this".to_string(), BSONString("is embedded".to_string()));
     embed.insert("negative".to_string(), BSONInt(-5));
@@ -97,4 +126,67 @@ fn embedded() {
                           0x6e,0x75,0x6d,0x62,0x65,0x72,0x00,
                           0x0A,0x00,0x00,0x00,
                           0x00));
+}
+
+#[test]
+fn deserialize_embedded() {
+    let mut embed = BSON::new();
+    embed.insert("this".to_string(), BSONString("is embedded".to_string()));
+    embed.insert("negative".to_string(), BSONInt(-5));
+    let mut correct = BSON::new();
+    correct.insert("hello".to_string(), BSONString("world".to_string()));
+    correct.insert("another".to_string(), BSONString("thing".to_string()));
+    correct.insert("number".to_string(), BSONInt(10));
+    correct.insert("embedded".to_string(), BSONObject(embed));
+    let built = BSON::deserialize(vec!(0x68,0x00,0x00,0x00,
+                                       0x02,
+                                       0x61,0x6e,0x6f,0x74,0x68,0x65,0x72,0x00,
+                                       0x06,0x00,0x00,0x00,
+                                       0x74,0x68,0x69,0x6e,0x67,0x00,
+                                       0x03,
+                                       0x65,0x6d,0x62,0x65,0x64,0x64,0x65,0x64,0x00,
+                                       0x29,0x00,0x00,0x00,
+                                       0x10,
+                                       0x6e,0x65,0x67,0x61,0x74,0x69,0x76,0x65,0x00,
+                                       0xFB,0xFF,0xFF,0xFF,
+                                       0x02,
+                                       0x74,0x68,0x69,0x73,0x00,
+                                       0x0c,0x00,0x00,0x00,
+                                       0x69,0x73,0x20,0x65,0x6d,0x62,0x65,0x64,0x64,0x65,0x64,0x00,
+                                       0x00,
+                                       0x02,
+                                       0x68,0x65,0x6c,0x6c,0x6f,0x00,
+                                       0x06,0x00,0x00,0x00,
+                                       0x77,0x6f,0x72,0x6c,0x64,0x00,
+                                       0x10,
+                                       0x6e,0x75,0x6d,0x62,0x65,0x72,0x00,
+                                       0x0A,0x00,0x00,0x00,
+                                       0x00));
+    assert_eq!(correct, built);
+}
+
+#[test]
+fn serialize_id() {
+    let mut bson = BSON::new();
+    bson.insert("_id".to_string(), BSONId(vec!(0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b)));
+    let serialized = bson.serialize();
+    show_bson(&serialized);
+    assert_eq!(serialized, vec!(0x16,0x00,0x00,0x00,
+                          0x07,
+                          0x5f,0x69,0x64,0x00,
+                          0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,
+                          0x00));
+}
+
+#[test]
+fn deserialize_id() {
+    let mut correct = BSON::new();
+    correct.insert("_id".to_string(), BSONId(vec!(0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b)));
+    // TODO - this works even with an incorrect length specified
+    let built = BSON::deserialize(vec!(0x16,0x00,0x00,0x00,
+                                       0x07,
+                                       0x5f,0x69,0x64,0x00,
+                                       0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,
+                                       0x00));
+    assert_eq!(correct, built);
 }
